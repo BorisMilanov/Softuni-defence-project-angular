c0526dc9e69d9f4ab1cc03d05294b85831488d08
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2022-12-06 13:20:33 +0200

    Sidebar added

--- client/loan-app/src/app/app.module.ts
+++ client/loan-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
