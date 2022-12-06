01796c4d419a69c25f4ceb210c8aec1c94b0e7b8
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2022-12-06 12:22:41 +0200

    Init routes and models 0.1

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/user');
+var customerRouter = require('./routes/customer');
+var depositRouter = require('./routes/deposit');
+var paymentRouter = require('./routes/payment');
 
 var app = express();
 
--- server/models/Customer.js
+++ server/models/Customer.js
@@ -1,9 +1,9 @@
 const { Schema, model } = require('mongoose');
 
 const customerSchema = new Schema({
-    firstname: 'String',
-    sencondName: 'String',
-    email: 'String'
+    firstname: String,
+    sencondName: String,
+    email: String
 });
 
 const Customer = model('Customer', customerSchema);
